4a741383ee5fa13d9642485b423a2c67f356841c
James Chen <jameschen@jamess-mbp.home>
2024-01-22 16:19:32 -0500

    changed to use env variables

--- data-source.ts
+++ data-source.ts
@@ -1,17 +1,18 @@
 import { DataSource } from "typeorm";
+import { config } from "dotenv";
+
+config(); // load variables from .env file
 
 export const AppDataSource = new DataSource({
    type: "postgres",
-   host: "localhost",
-   port: 5432,
-   username: "test",
-   password: "test",
-   database: "test",
+   host: process.env.DB_HOST,
+   port: Number(process.env.DB_PORT),
+   username: process.env.DB_USER,
+   password: process.env.DB_PASS,
+   database: process.env.DB_NAME,
    synchronize: true,
    logging: false,
    entities: [
-      "src/entity/**/*.ts"
+      "src/model/*.ts"
    ]
-})
-
-  
+});
